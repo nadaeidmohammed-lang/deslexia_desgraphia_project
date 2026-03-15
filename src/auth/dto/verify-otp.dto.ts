7bfe0f1945103ce02bd7e7ba7d4abc6ceff18f75
Omar Khaled <omarkhaaleedd@gmail.com>
2026-03-16 00:54:38 +0200

    otp

--- src/auth/dto/verify-otp.dto.ts
+++ src/auth/dto/verify-otp.dto.ts
@@ -2,11 +2,17 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class VerifyOtpDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'User email address',
+  })
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '1234',
+    description: 'OTP code sent to email',
+  })
   @IsString()
   otp: string;
 }
